d680aa747960c794c56b201f348b25938e0558fa
satse13 <atobiasangeles@gmail.com>
2023-11-13 19:56:00 +0100

    Fix error in exercise 6

--- Reto #06 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK/solucion.cpp
+++ Reto #06 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK/solucion.cpp
@@ -8,39 +8,37 @@ void method(const vector<tuple<string,string>> &tupList){
 	
 	int p1 = 0;
 	int p2 = 0;
-	for(int i = 0; i < 3;i++){
-		for(tuple t: tupList){
-			if(get<0>(t) == "🗿" && ((get<1>(t) == "✂️") || (get<1>(t) == "🦎"))){
-				p1++;
-				continue;
-			}
-			else if(get<0>(t) == "✂️" && ((get<1>(t) == "🦎") || (get<1>(t) == "📄"))){
-				p1++;
-				continue;
-			}
-			else if(get<0>(t) == "📄" && ((get<1>(t) == "🗿") || (get<1>(t) == "🖖"))){
-				p1++;
-				continue;
-			}
-			else if(get<0>(t) == "🦎" && ((get<1>(t) == "📄") || (get<1>(t) == "🖖"))){
-				p1++;
-				continue;
-			}
-			else if(get<0>(t) == "🖖" && ((get<1>(t) == "✂️") || (get<1>(t) == "🗿"))){
-				p1++;
-				continue;
-			}
-			else {
-				p2++;
-				continue;
-			}
+	for(tuple t: tupList){
+		if(get<0>(t) == "🗿" && ((get<1>(t) == "✂️") || (get<1>(t) == "🦎"))){
+			p1++;
+			continue;
+		}
+		else if(get<0>(t) == "✂️" && ((get<1>(t) == "🦎") || (get<1>(t) == "📄"))){
+			p1++;
+			continue;
+		}
+		else if(get<0>(t) == "📄" && ((get<1>(t) == "🗿") || (get<1>(t) == "🖖"))){
+			p1++;
+			continue;
+		}
+		else if(get<0>(t) == "🦎" && ((get<1>(t) == "📄") || (get<1>(t) == "🖖"))){
+			p1++;
+			continue;
+		}
+		else if(get<0>(t) == "🖖" && ((get<1>(t) == "✂️") || (get<1>(t) == "🗿"))){
+			p1++;
+			continue;
+		}
+		else {
+			p2++;
+			continue;
 		}
 	}
-
+	
 	if(p1 > p2)
 		cout << "P1 wins" << endl;
 	else if(p2 > p1)
-		cout << "P2 winds" << endl;
+		cout << "P2 wins" << endl;
 	else 
 		cout << "TIE" << endl;
 }
